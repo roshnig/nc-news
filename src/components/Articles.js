538d122c78af90eff885cb095f9300804cb55d32
Roshni <roshnigupta8@gmail.com>
2022-02-10 16:11:52 +0000

    done css change for article detail

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -24,7 +24,7 @@ const Articles = () => {
             <nav className="articles_header">
                 <h2>{(topic) ? topic : 'Trending News'}</h2>
                 <div className="articles_header_sortdiv">
-                    <label>SortBy :- </label>
+                    <label>SortBy- </label>
                     <select onChange={(event) => { setSortBy(event.target.value) }}>
                         <option value='created_at'>Article Date</option>
                         <option value='author'>Author</option>
@@ -33,21 +33,21 @@ const Articles = () => {
                     </select>
                 </div>
             </nav>
-            <ScrollContainer>
-                <section className="articles_maindiv">
+            {/* <ScrollContainer> */}
+            <section className="articles_maindiv">
 
-                    {articles.map((article) => {
-                        return (
-                            <div key={article.article_id} className="art_cards">
-                                <div className="art_cards_main">
-                                    <h4>{article.topic}</h4>
-                                    <div className="art_cards_contents">
-                                        <p>{article.title}</p>
-                                        <div className="art_cards_contents_author">
-                                            <p>{article.author}</p>
-                                            <p>{article.created_at.split('T')[0]}</p>
-                                        </div>
-                                        {/* <div className="art_card_footer">
+                {articles.map((article) => {
+                    return (
+                        <div key={article.article_id} className="art_cards">
+                            <div className="art_cards_main">
+                                <h4>{article.topic}</h4>
+                                <div className="art_cards_contents">
+                                    <p>{article.title}</p>
+                                    <div className="art_cards_contents_author">
+                                        <p>{article.author}</p>
+                                        <p>{article.created_at.split('T')[0]}</p>
+                                    </div>
+                                    {/* <div className="art_card_footer">
                                         <p>Votes: {article.votes}</p>
                                         <Link
                                             className="articles_maindiv_moredetail"
@@ -56,24 +56,24 @@ const Articles = () => {
                                             More Detail
                                         </Link>
                                     </div> */}
-                                    </div>
-                                    <div className="art_card_footer">
-                                        <p>Votes: {article.votes}</p>
-                                        <Link
-                                            className="articles_maindiv_moredetail"
-                                            to={`/articles/${article.article_id}`}
-                                        >
-                                            More Detail
-                                        </Link>
-                                    </div>
                                 </div>
-
+                                <div className="art_card_footer">
+                                    <p>Votes: {article.votes}</p>
+                                    <Link
+                                        className="articles_maindiv_moredetail"
+                                        to={`/articles/${article.article_id}`}
+                                    >
+                                        More Detail
+                                    </Link>
+                                </div>
                             </div>
-                        )
-                    })}
 
-                </section>
-            </ScrollContainer>
+                        </div>
+                    )
+                })}
+
+            </section>
+            {/* </ScrollContainer> */}
             <div className="pagination"><Pagination count={6} shape="rounded" onChange={(evt, value) => { setPage(value) }} /></div>
         </div>
     )
